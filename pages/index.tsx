a615a0be72849a0bc9e79bfcc49e97af85187738
marco0antonio0 <marcomesquitajr@hotmail.com>
2025-02-15 14:52:36 -0300

    feat/GENAudio-4: melhorando a tela de home

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,10 @@ import {default as ImageNext} from "next/image";
 
 import localFont from "next/font/local";
 import UserShowElement from "@/components/userShowElement";
-import { Button, Image, image } from "@heroui/react";
+import { Avatar, Badge, Button, Image, image } from "@heroui/react";
+import { useRouter } from "next/router";
+import SectionPromo from "@/components/section/promo";
+import SectionPost from "@/components/section/post";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -14,14 +17,16 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+{/* <div className="bg-blue-500 w-[100%] h-[500px]"></div> */}
 
 export default function Home() {
+  const r = useRouter()
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid min-h-screen py-3 pb-20 gap-16 sm:px-10  font-[family-name:var(--font-geist-sans)]`}
     >
         <div className="flex flex-col max-w-[1000px] smi:px-10 smi:mt-[-40px]">
-          <img src="/promo/logo.png" alt="" className="w-[350px] m-auto mdi:w-[300px]"/>
+          <img src="/promo/4159173.jpg" alt="" className="w-[350px] m-auto mdi:w-[300px]"/>
         <h1 className="m-auto mt-[10px] text-[7mm] font-semibold text-[#8287eb] smi:text-[6mm]">Dê vida aos seus textos</h1>
         <h1 className="m-auto mt-[0] text-[10mm] font-semibold text-black text-center smi:text-[8mm]">Em poucos cliques crie audios facilmente </h1>
         <span className="m-auto mt-[10px] text-lg font-normal opacity-65 text-black text-justify">Você pode usar nosso gerador de audios personalizados de maneira facil. Vamos lá!</span>
@@ -29,41 +34,32 @@ export default function Home() {
         <Button color="primary" variant="shadow" className="w-[250px] h-[60px] lgi:w-[190px] lgi:h-[60px]">
         Comece gratuitamente
       </Button>
-      <Button color="primary" variant="ghost" className="w-[250px] h-[60px] lgi:w-[190px] lgi:h-[60px]">
+      <Button color="primary" variant="ghost" className="w-[250px] h-[60px] lgi:w-[190px] lgi:h-[60px]" onClick={()=>{r.push('/sobre')}}>
         Saiba mais
       </Button>
         </div>
-        <div className="flex flex-row gap-8 mt-16 mdi:flex-col">
-          <div className="w-[50%] mdi:w-[100%]">
-          <p className="text-lg text-justify">
-        Desde o início, nossa missão tem sido simplificar a criação de áudios personalizados, tornando essa tecnologia acessível para todos. Com apenas alguns cliques, você pode transformar qualquer texto em uma narração profissional, sem complicações.
-
-Nosso gerador de áudio foi projetado para ser intuitivo, ágil e eficiente, permitindo que você crie conteúdos sonoros com naturalidade e qualidade impressionante. Seja para vídeos, apresentações, acessibilidade ou qualquer outra necessidade, estamos aqui para tornar esse processo simples e envolvente.
-
-Vamos começar? Transforme suas palavras em voz agora mesmo! 🚀🎙️
-        </p>
-          </div>
-          <div className="m-auto">
-            <Image
-              isBlurred
-              alt="HeroUI Fruit Image with Zoom"
-              src="/promo/promoinicio.png"
-              className="w-auto h-[350px] m-auto mr-0 lgi:h-[450px]"
-            />
-          </div>
+        {/* <img src="/promo/4159173.jpg" alt="" className="w-[100%] max-w-[400px] m-auto"/> */}
+        
+        <div className="flex flex-row-reverse gap-8 mt-16 mdi:flex-col">
+          <SectionPromo/>
         </div>
 
+        <div className="flex flex-row gap-8 mt-3 mdi:flex-col">
+          <SectionPost src={"/promo/promoinicio.png"} text={`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim consequat sem a ornare. Aenean vel nunc elementum magna feugiat vehicula. Nunc molestie rutrum neque. Sed feugiat dictum enim imperdiet pulvinar. Proin dolor purus, vestibulum ac tortor eget, scelerisque dictum urna. Integer gravida quis ex at tincidunt. Vivamus id justo neque.
+
+Ut luctus libero ac ligula placerat porta. Nulla ut magna sed leo porttitor posuere. Nam feugiat ut metus et porttitor. Vestibulum ut aliquam sem. Aliquam et nulla massa. `}/>
+        </div>
         <div className="h-5"></div>
-        <h1 className="m-auto ml-0 mt-[10px] text-[7mm] font-semibold text-[#8287eb] smi:text-[6mm]">Criadores do GENAudio</h1>
+        <h1 className="m-auto ml-0 mt-[10px] text-[7mm] font-semibold text-[#8287eb] smi:text-[6mm]">Criadores do GENAudio</h1> 
         <div className="h-2"></div>
         <div className="flex flex-wrap flex-row gap-3">
         <UserShowElement url="https://github.com/marco0antonio0" name="Marco Antonio" username="marco0antonio0" src="https://github.com/marco0antonio0.png?size=100px" text="Entusiasta da tecnologia e inovação, sempre em busca de soluções eficientes e criativas no desenvolvimento de software."/>
         <UserShowElement url="https://github.com/EliasPacheco" name="Elias Pacheco" username="EliasPacheco" src="https://github.com/EliasPacheco.png?size=100px" text="Desenvolvedor de software focado em criar experiências digitais inteligentes e impactantes, unindo tecnologia e criatividade. 🚀"/>
         </div>
         <p className="text-lg text-justify mt-10">
-        A acessibilidade e a inovação caminham juntas no GENAudio. Transforme qualquer texto em narrações envolventes e naturais com apenas alguns cliques.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim consequat sem a ornare. Aenean vel nunc elementum magna feugiat vehicula. Nunc molestie rutrum neque. Sed feugiat dictum enim imperdiet pulvinar. Proin dolor purus, vestibulum ac tortor eget, scelerisque dictum urna. Integer gravida quis ex at tincidunt. Vivamus id justo neque.
 
-Crie áudios para treinamentos, vídeos explicativos, conteúdos educacionais ou qualquer outra necessidade. Com nossa plataforma, a sua voz ganha vida de forma simples e intuitiva. Comece agora mesmo! 🎧✨
+Ut luctus libero ac ligula placerat porta. Nulla ut magna sed leo porttitor posuere. Nam feugiat ut metus et porttitor. Vestibulum ut aliquam sem. Aliquam et nulla massa.
         </p>
         </div>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
@@ -116,14 +112,6 @@ Crie áudios para treinamentos, vídeos explicativos, conteúdos educacionais ou
     </div>
   );
 }
-
-
-
-
-
-
-
-
 
 
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -49,6 +49,44 @@ const config: Config = {
     },
   },
   darkMode: "class",
-  plugins: [heroui()],
+  plugins: [ heroui({
+    themes: {
+      light: {
+        colors: {
+          background: "#FFFFFF", // or DEFAULT
+          foreground: "#11181C", // or 50 to 900 DEFAULT
+          primary: {
+            //... 50 to 900
+            foreground: "#FFFFFF",
+            DEFAULT: "#006FEE",
+          },
+          // ... rest of the colors
+        },
+      },
+      dark: {
+        colors: {
+          background: "#000000", // or DEFAULT
+          foreground: "#ECEDEE", // or 50 to 900 DEFAULT
+          primary: {
+            //... 50 to 900
+            foreground: "#FFFFFF",
+            DEFAULT: "#006FEE",
+          },
+        },
+        // ... rest of the colors
+      },
+      mytheme: {
+        // custom theme
+        extend: "dark",
+        colors: {
+          primary: {
+            DEFAULT: "#BEF264",
+            foreground: "#000000",
+          },
+          focus: "#BEF264",
+        },
+      },
+    },
+  }),],
 };
 export default config;
